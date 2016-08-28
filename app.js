abeafe9b73f28149cce880f9839796c220f678d4
Nikhil Kejirwal <Nikhil Kejirwal>
2016-08-28 15:13:11 +0530

    mastkalandar

--- app.js
+++ app.js
@@ -96,15 +96,20 @@ app.post('/user-put', function(req, res){
         MongoClient.connect(config.mongo_url, function(err, db) {
             db.collection("troope_users").updateOne(
                 { "user_id" : req.body.user_id },
-                { $set: { data["address"]:req.body.address,
-                        data["bank_no"] :req.body.bank_no,
-                        data["current_company"] : req.body.current_company,
-                        data["job_title"] : req.body.job_title,
-                        data["freelancing"] : req.body.freelancing,
-                        data["bank_ifsc"]:req.body.bank_ifsc,
-                        data["looking_projects"]: req.body.looking_projects,
-                        data.["mobile"]:req.body.mobile
-                 } }
+                { $set: {"data.address":req.body.address,
+                        "data.bank_no" :req.body.bank_no,
+                        "data.current_company" : req.body.current_company,
+                        "data.job_title" : req.body.job_title,
+                        "data.freelancing" : req.body.freelancing,
+                        "data.bank_ifsc":req.body.bank_ifsc,
+                        "data.looking_projects": req.body.looking_projects,
+                        "data.mobile":req.body.mobile
+                 } }{upsert:true},function(err,result){
+                    if(err) throw err;
+                    else {
+                      console.log("result update one>> ",result);
+                    }
+                 }
             )
         });
         res.render('./views/index.ejs',{
