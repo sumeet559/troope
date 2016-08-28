8ed212573feabe2aa25e4595b16c1ebb53e96514
Nikhil Kejirwal <Nikhil Kejirwal>
2016-08-28 14:57:06 +0530

    sahi hogya hayad

--- app.js
+++ app.js
@@ -90,6 +90,28 @@ app.get('/logout', function(req, res){
   res.redirect('/');
 });
 
+app.post('/user-put', function(req, res){
+   console.log("In Submission POST")
+        var user_data = "";
+        MongoClient.connect(config.mongo_url, function(err, db) {
+            db.collection("troope_users").updateOne({
+                { "user_id" : req.body.user_id },
+                { $set: { data.address:req.body.address,
+                        data.bank_no :req.body.bank_no,
+                        data.current_company : req.body.current_company,
+                        data.job_title : req.body.job_title,
+                        data.freelancing : req.body.freelancing,
+                        data.bank_ifsc:req.body.bank_ifsc,
+                        data.looking_projects: req.body.looking_projects,
+                        data.mobile:req.body.mobile
+                 } }
+            })
+        });
+        res.render('./views/index.ejs',{
+            Test: TestType
+        });
+});
+
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
