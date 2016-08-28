3e101be13abc6ade78b4ab31216f22120f710f57
sumeet tiwari <sumeet.bitsg@gmail.com>
2016-08-28 11:14:06 +0530

    Update config.js

--- configuration/config.js
+++ configuration/config.js
@@ -1,6 +1,6 @@
 module.exports={
-	"facebook_api_key" 		: 	"1610327349260171",
-	"facebook_api_secret"	:	"3c5949d29b9bd21782845c196308aa3f",
+	"facebook_api_key" 		: 	"1774612552779075",
+	"facebook_api_secret"	:	"af310d1bef52af58f0f214fd479cde3d",
 	"callback_url"			:	"http://52.38.150.187:3000/auth/facebook/callback",
 	"use_database"			:	"true",
 	"mongo_url"             :  "mongodb://nikhil:nikhil@1111@ds013216.mlab.com:13216/troope"
